374ecbe6bf16131471f6b60c6c9f76b89bf540cf
VINGROUP\kienmt1 <kienmt1@vingroup.net>
2020-10-07 17:03:51 +0700

    Updates

---
+++ precache-manifest.a66624bf2a43d1e4c4ee68642cfdab54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc5eb05b69b7d6d2f3c770f0e4fed44",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "05d5ff08f716dfbf1e25",
+    "url": "/test/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/test/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d5ff08f716dfbf1e25",
+    "url": "/test/static/js/main.1cc0fee2.chunk.js"
+  },
+  {
+    "revision": "0d6dc1693978d1d8b578",
+    "url": "/test/static/js/runtime-main.cce556a0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/test/static/media/logo.ee7cd8ed.svg"
+  }
+]);
